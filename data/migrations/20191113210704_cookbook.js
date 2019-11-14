dd62b1165c4d2b20088c954a427fe152e2251066
Caleb Redd <caleb.redd@yahoo.com>
2019-11-13 21:22:32 -0700

    Many to many relationships between recipes and both ingredients and steps

--- data/migrations/20191113210704_cookbook.js
+++ data/migrations/20191113210704_cookbook.js
@@ -2,12 +2,50 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("recipes", tbl => {
       tbl.increments();
+      tbl.text("recipe_name", 128).notNullable();
     })
     .createTable("ingredients", tbl => {
       tbl.increments();
+      tbl.text("name", 128).notNullable();
+    })
+    .createTable("steps", tbl => {
+      tbl.increments();
+      tbl.text("name", 128).notNullable();
+      tbl.text("description",128).notNullable();
     })
     .createTable("recipes_ingredients", tbl => {
       tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .references("id")
+        .inTable("recipes")
+        .notNullable()
+        .unsigned();
+      tbl
+        .integer("ingredients_id")
+        .references("id")
+        .inTable("ingredients")
+        .notNullable()
+        .unsigned();
+      tbl
+        .float("quantity")
+        .notNullable()
+        .unsigned();
+    })
+    .createTable("recipes_steps", tbl => {
+      tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .references("id")
+        .inTable("recipes")
+        .notNullable()
+        .unsigned();
+      tbl
+        .integer("steps_id")
+        .references("id")
+        .inTable("steps")
+        .notNullable()
+        .unsigned();
     });
 };
 
