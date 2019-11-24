0028b031a6e3da1d8e09782ed3456ee57aeb5baf
Caleb Redd <caleb.redd@yahoo.com>
2019-11-23 18:03:23 -0700

    Correct DB routing, Router file complete

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./dev.sqlite3"
+      filename: "./data/recipeBook.sqlite3"
     },
     migrations: {
       directory: "./data/migrations"
--- recipes/routes.js
+++ recipes/routes.js
@@ -9,12 +9,79 @@ router.get("/api", (req, res) => {
   res.send("<h2>Welcome to the Recipe Book API</h2>");
 });
 router.get("/api/recipes", (req, res) => {
-  db.get()
+  db.find()
     .then(recipes => {
       res.status(200).json(recipes);
     })
-    .catch(err => {
+    .catch(() => {
       res.status(500).json({ errorMessage: "Unable to access database" });
+    });
+});
+router.get("/api/recipes/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(recipe => {
+      res.status(200).json(recipe);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to access recipe details" });
+    });
+});
+router.post("/api/recipes", (req, res) => {
+  const addRecipe = req.body;
+  db.insert(addRecipe)
+    .then(() => {
+      res.status(200);
+      db.find()
+        .then(recipes => {
+          res.status(200).json(recipes);
+        })
+        .catch(() => {
+          res.status(500).json({ errorMessage: "Unable to access database" });
+        });
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "Unable to add recipe to database" });
+    });
+});
+router.put("/api/recipes/:id", (req, res) => {
+  const editRecipe = req.body;
+  db.update(req.params.id, editRecipe)
+    .then(recipe => {
+      res.status(200).json(recipe);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to access database" });
+    });
+});
+router.delete("/api/recipes/:id", (req, res) => {
+  db.remove(req.params.id)
+    .then(recipe => {
+      res.status(200).json(recipe);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "Unable to remove Recipe from database" });
+    });
+});
+router.get("/api/recipes/:id/shoppingList", (req, res) => {
+  db.findIngredients(req.params.id)
+    .then(ingredients => {
+      res.status(200).json(ingredients);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to access Shopping List" });
+    });
+});
+router.get("/api/recipes/:id/instructions", (req, res) => {
+  db.findSteps(req.params.id)
+    .then(Steps => {
+      res.status(200).json(Steps);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to access Instructions" });
     });
 });
 
